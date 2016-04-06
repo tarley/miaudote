a553894bc32a620fbe60647a8ec1b796b6c711d8
Gleidson Guimarães Moura <gleidson.gmoura@gmail.com>
2016-04-06 16:58:51 -0300

    1 - Colocar espaço entre as figuras [set.css - .grid figure] margin de tres pixels e reduzir os pixels do tamanho total da imagem
    2 - criar estilo para os itens da paginação [set.css .nav-pagination] no
    centro com dez pixels e a fonte monospace
    
    3 - criar funções no util.js para aplicar a paginação e controlar os
    eventos

--- assets/php/conexao.php
+++ assets/php/conexao.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'DB.php';
 function conectar(){
-	$pdo= new PDO('mysql:host=localhost;dbname=adote','root','1234');
+	$pdo= new PDO('mysql:host=localhost;dbname=adote','root','');
 	return $pdo;
 }
 ?>
