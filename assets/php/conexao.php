89c58d5626e6ecbbfd28190b9f68563f66a11e85
Tarley <tarley.lana@gmail.com>
2016-04-09 21:59:30 -0300

    Criando um unico ponto de alteração do banco de dados. Utilizando as variaveis globais definidas em config.php.

--- assets/php/conexao.php
+++ assets/php/conexao.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'DB.php';
 function conectar(){
-	$pdo= new PDO('mysql:host=localhost;dbname=adote','root','');
+	$pdo= new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 	return $pdo;
 }
 ?>
