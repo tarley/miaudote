196954d0e9d6b087cdbc7eb5e1f9f84d2c7fce97
Luiz Henrique Boggione Ferreira <luizboggione@gmail.com>
2016-05-06 11:07:54 -0300

    Exibição das fotos na aprovação dos animais

--- aprovacao.php
+++ aprovacao.php
@@ -160,7 +160,7 @@ $lista = $pdo->query ( "select a.cod_animal,
 													while ( $row = $lista->fetch ( PDO::FETCH_ASSOC ) ) {
 														echo "<tr class='tbl-item'>													
 															<td class='img'>
-																<a href='#'><p class='desc'>Ver Fotos</p></a>
+																<a href='' onclick='AbrirFotos(" .$row["cod_animal"]. ");'><p class='desc'>Ver Fotos</p></a>
 																<img src='" .$row["url"]. "' alt='' title='' />							   									
 															</td>													
 															<td class='td-block'>
@@ -301,21 +301,9 @@ $lista = $pdo->query ( "select a.cod_animal,
 
 <!--CORE JAVASCRIPT-->
 <script src="assets/js/aprovacao/main.js"></script>
-<script>        (function (i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function () {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-        ga('create', 'UA-145464-12', 'auto');
-        ga('send', 'pageview');
-
-
+<script type="text/javascript">
+	function AbrirFotos(id) {		
+		var win = window.open('foto.php?id='+id,'_blank');
+	}
 </script>
-
 </html>
---
+++ foto.php
@@ -0,0 +1,123 @@
+<?php
+Error_reporting ( 0 );
+
+include ("assets/php/conexao.php");
+$pdo = conectar ();
+
+header ( "Content-Type: text/html; charset=UTF-8", true );
+
+$fotos = $pdo->query ( "select url
+						  from 
+							tb_foto
+						  where cod_animal = " . $_GET ["id"] . "
+							and id_foto_pri = 'N'" );
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Galeria de Fotos</title>
+<link href="./assets/css/aprovacao/bootstrap.min.css" rel="stylesheet">
+<style>
+@import url(https://fonts.googleapis.com/css?family=Bree+Serif);
+
+body {
+	background: #ebebeb;
+}
+
+ul {
+	padding: 0 0 0 0;
+	margin: 0 0 40px 0;
+}
+
+ul li {
+	list-style: none;
+	margin-bottom: 10px;
+}
+
+ul li.bspHasModal {
+	cursor: pointer;
+}
+
+.modal-body {
+	padding: 5px !important;
+}
+
+.modal-content {
+	border-radius: 0;
+}
+
+.modal-dialog img {
+	text-align: center;
+	margin: 0 auto;
+}
+
+.controls {
+	width: 50px;
+	display: block;
+	font-size: 11px;
+	padding-top: 8px;
+	font-weight: bold;
+}
+
+.next {
+	float: right;
+	text-align: right;
+}
+
+.text {
+	font-family: 'Bree Serif';
+	color: #666;
+	font-size: 11px;
+	margin-bottom: 10px;
+	padding: 12px;
+	background: #fff;
+}
+
+.glyphicon-remove-circle:hover {
+	cursor: pointer;
+}
+
+@media screen and (max-width: 380px) {
+	.col-xxs-12 {
+		width: 100%;
+	}
+	.col-xxs-12 img {
+		width: 100%;
+	}
+}
+</style>
+</head>
+<body>
+	<br/>
+	<div class="container">
+	<?php
+	if ($fotos) {
+		while ( $row = $fotos->fetch ( PDO::FETCH_ASSOC ) ) {
+			echo "<ul class='row first'>		
+					<li>
+						<img src='" . $row ["url"] . "'>
+					</li>				
+				  </ul>";
+		}
+	}
+	?>
+	</div>
+</body>
+<script src="./assets/js/aprovacao/jquery-1.10.2.min.js"></script>
+<script src="./assets/js/aprovacao/bootstrap.min.js"></script>
+<script src="./assets/js/aprovacao/jquery.bsPhotoGallery.js"></script>
+<script>
+      $(document).ready(function(){
+        $('ul.first').bsPhotoGallery({
+          "classes" : "col-lg-2 col-md-4 col-sm-3 col-xs-4 col-xxs-12",
+          "hasModal" : true
+        });
+      });
+    </script>
+</html>
