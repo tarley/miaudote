6b719efc0676b094eba628b24c7e58c0742e0417
Bruna <Bruna@DESKTOP-QQCCP2P>
2016-05-07 16:59:24 -0300

    Funçao de Segurança

---
+++ seguranca.php
@@ -0,0 +1,32 @@
+<?php
+function protectPage() {
+	global $_SG;
+	
+	if (! isset ( $_SESSION ['usuarioID'] ) or ! isset ( $_SESSION ['usuarioNome'] )) {
+		// Não há usuário logado, manda pra página de login
+		kick_out ();
+	} else if (! isset ( $_SESSION ['usuarioID'] ) or ! isset ( $_SESSION ['usuarioNome'] )) {
+		// Há usuário logado, verifica se precisa validar o login novamente
+		if ($_SG ['validaSempre'] == true) {
+			// Verifica se os dados salvos na sessão batem com os dados do banco de dados
+			if (! validaUsuario ( $_SESSION ['usuarioLogin'], $_SESSION ['usuarioSenha'] )) {
+				// Os dados não batem, manda pra tela de login
+				kick_out ();
+			}
+		}
+	}
+}
+
+/**
+ * Função para expulsar um visitante
+ */
+function kick_out() {
+	global $_SG;
+	
+	// Remove as variáveis da sessão (caso elas existam)
+	unset ( $_SESSION ['usuarioID'], $_SESSION ['usuarioNome'], $_SESSION ['usuarioLogin'], $_SESSION ['usuarioSenha'] );
+	
+	// Manda pra tela de login
+	header ( "Location:../../index.php?page=login&tentativa=tentei" );
+}
+?>
