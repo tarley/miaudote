82cc6bc0646e28683bfc128490db40bba597d069
Jef <Jef@192.168.15.4>
2016-05-08 13:33:55 -0300

    arquivo php externo responsavel por exibir o perfil do animal selecionado

---
+++ perfil.php
@@ -0,0 +1,100 @@
+<?php
+	//error_reporting(E_ALL);
+	if(!isset($pdo)){
+		require_once 'assets/php/conexao.php';
+		$pdo = conectar();
+		
+	}
+	
+	$identificador = $_GET['id'];
+	try{	
+		$sql = "select 	a.cod_animal,
+				a.nom_animal,
+				a.desc_perfil,
+				a.idade,
+				f.cod_foto,
+				f.nom_foto,
+				u.telefone,
+				u.email,
+				
+				case 
+				  when ind_porte = 1 then 'pequeno'
+				  when ind_porte = 2 then 'medio'
+				  when ind_porte = 3 then 'grade'
+				  when ind_porte = 4 then 'gigante'
+				  else 'indefinido'
+
+
+				end as ind_porte,
+				case 
+				  when ind_sexo  = 1 then 'femea'
+				  when ind_sexo  = 2 then 'macho'
+				  else 'indefinido'
+
+				end as ind_sexo							
+		from 	tb_animal a, tb_foto f,tb_usuario u
+		where 	a.cod_animal = ?
+		and     a.cod_usuario = u.cod_usuario
+		and     a.cod_animal = f.cod_animal and
+				f.id_foto_pri = 's'";
+
+		$resultado = $pdo->prepare($sql);
+		$resultado->execute(array($identificador));
+	}
+    catch(PDOException $e) {
+        // imprimimos a nossa excecao
+        echo $e->getMessage();
+    }
+	
+?>
+
+
+<div id='perfil-pet'>
+	<div class='panel panel-default'>
+		<div class='panel-body'>
+			<?php
+				if($resultado){
+					while($row = $resultado->fetch()) {
+						echo"<div class='drop-shadow lifted' style='position:relative;margin:0 auto;width:800px;min-height:500px;border:0px solid';>
+								<img  class='img-responsive' alt='#' id='details-img' src='images/profile/user_1/pet_$row[cod_animal]/book/$row[nom_foto].jpg'/>
+							</div>
+							<div id='content'>
+								<ul id='tabs' class='nav nav-tabs' data-tabs='tabs'>
+									<li class='active'><a href='#sobre-mim' data-toggle='tab'>Sobre Min</a></li>
+									<li><a href='#caracteristicas' data-toggle='tab'>Caracteristicas</a></li>
+									<li><a href='#contato' data-toggle='tab'>Contato</a></li>
+								</ul>
+								<div id='my-tab-content' class='tab-content' style ='min-height:100px'>
+									<div class='tab-pane active' id='sobre-mim'>
+										
+										<p>$row[desc_perfil]</p>
+									</div>
+									<div class='tab-pane' id='caracteristicas'>
+										<ul>
+										<li>Idade: $row[idade]. ano(s) </li>
+										<li>Sexo : $row[ind_porte]  </li>
+										<li>Porte: $row[ind_sexo] </li>
+										</ul>	
+									</div>
+									<div class='tab-pane' id='contato'>
+										<ul>
+											<li>Telefone:$row[telefone]</li>
+											<li>E-mail : $row[email]  </li>
+										</ul>		
+									</div>
+								</div>
+							</div>";
+					}
+					// Desconecta
+				$pdo = null;
+				}else{
+					echo'Dados não encontrados';
+				}
+		?>
+		</div>
+	</div>
+</div>
+
+
+
+
