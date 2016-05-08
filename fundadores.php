a1cd506c3356624f3e85810b886e8d36426bc612
Jef <Jef@192.168.15.4>
2016-05-08 13:37:36 -0300

    peuquenas alterações na pagina de fundadores

--- fundadores.php
+++ fundadores.php
@@ -1,60 +1,36 @@
-<html lang="en">
-<?php include 'shared/_header.php';?>
 
-	<link rel="stylesheet" href="assets/css/fundadores/fundadores.css">
-	<!-- link rel="stylesheet" href="assets/css/fundadores/agency.css"-->
-<body>
-	<?php
-	
-	include 'shared/_menu.php';
-	include 'assets/php/fundador.php';
-	
-	$fundadoresSI = array (
-			new Fundador ( 'Ariane Silva', 'Analista de testes', 'Ariane_Silva.jpg', '', '', '' ),
-			new Fundador ( 'Bruna Frazoni', 'Desenvolvedora', 'Bruna_Frazoni.jpg', '', '', '' ),
-			new Fundador ( 'Cibele Castelo', 'Product Owner', 'Cibele_Castelo.jpg', '', '', '' ),
-			new Fundador ( 'Igor Yuri', 'Product Owner', 'Igor_Yuri.jpg', '', '', '' ),
-			new Fundador ( 'Juliano Savio', 'Desenvolvedor', 'Juliano_Savio.jpg', '', '', '' ),
-			new Fundador ( 'Lucas Raphael', 'Desenvolvedor', 'Lucas_Raphael.JPG', '', '', '' ),
-			new Fundador ( 'Luiz Henrique', 'Desenvolvedor', 'Luiz_Henrique.jpg', '', '', '' ),
-			new Fundador ( 'Marcela SimÃ£o', 'Desenvolvedora', 'Marcela_Simao.jpg', '', '', '' ),
-			new Fundador ( 'Felipe Sangiorge', 'Analista de testes', '', '', '', '' ),
-			new Fundador ( 'Albert Rocha', 'Analista de testes', '', '', '', '' ),
-			new Fundador ( 'Gustavo Resende', 'Administrador de Banco de Dados', '', '', '', '' ),
-			new Fundador ( 'Carlos Alberto', 'Desenvolvedor', 'Carlos_Alberto.jpg', '', '', '' ),
-			new Fundador ( 'Jeferson EustÃ¡quio', 'Desenvolvedor', '', '', '', '' ),
-			new Fundador ( 'Tarley Lana', 'Prof.Orientador', 'Tarley_Lana.jpg', '', '', '' ),
-	);
-	
-	$fundadoresVet = array (
-			new Fundador ( 'Paula', 'Prof(a).Orientadora', '', '', '', '' ) 
-	);
-	
-	$disciplinas = array (
-			new Disciplina ( "Sistemas de InformaÃ§Ã£o - 8Âº PerÃ­odo", $fundadoresSI ),
-			new Disciplina ( "VeterinÃ¡ria - 8Âº PerÃ­odo", $fundadoresVet ) 
-	);
-	
+	<?php 
+		if(isset($fundadores)){
+			include 'assets/php/fundador.php';
+		}
+	      	
 	?>
 	
 	<div class="container container-fundador">
 		<div class="row">
 			<div class="col-lg-12 text-center">
 				<h2 class="section-heading"><?php echo utf8_encode('Fundadores do Sistema')?></h2>
-			</div>
-		</div>
-		<?php foreach($disciplinas as $d) :?>
-		<div class="row">
-			<div class="col-lg-12 text-center">
-				<h3 class="section-subheading text-muted"><?php echo $d->nome ?></h3>
+				<h3 class="section-subheading text-muted"><?php echo utf8_encode('Sistemas de Informação -	8º Período')?></h3>
 			</div>
 		</div>
 		<div class="row">
-			<?php foreach ( $d->fundadores as $f ) :?>
+			<?php 
+			
+				$fundadores = array(
+						new Fundador('Adelaide Antunes', 'Desenvolvedora', '', '', '', ''),
+						new Fundador('Adelaide Antunes', 'Desenvolvedora', '', '', '', ''),
+						new Fundador('Adelaide Antunes', 'Desenvolvedora', '', '', '', ''),
+						new Fundador('Adelaide Antunes', 'Desenvolvedora', '', '', '', ''),
+						new Fundador('Adelaide Antunes', 'Desenvolvedora', '', '', '', '')
+				);	
+				
+				foreach ($fundadores as $f) :
+			?>
 		
 			<div class="col-sm-3">
 				<div class="team-member">
-					<img class="img-responsive img-circle img-fundadores" alt="Imagem <?php $f->nome?>" src="<?php echo $f->urlImagem?>">
+					<img class="img-responsive img-circle img-fundadores" alt=""
+						src="<?php echo $f->urlImagem?>">
 					<h4><?php echo $f->nome?></h4>
 					<p class="text-muted"><?php echo $f->papel?></p>
 					<ul class="list-inline social-buttons">
@@ -66,16 +42,37 @@
 			</div>
 			<?php endforeach;?>
 		</div>
-		<?php endforeach;?>		
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<h3 class="section-subheading text-muted"><?php echo utf8_encode('Sistemas de Informação - 4º Período')?></h3>
+			</div>
+			
+			<?php 
+			
+				$fundadores = array(
+						new Fundador('Adelaide Antunes', 'Desenvolvedora', '', '', '', ''),
+						new Fundador('Adelaide Antunes', 'Desenvolvedora', '', '', '', ''),
+						new Fundador('Adelaide Antunes', 'Desenvolvedora', '', '', '', ''),
+						new Fundador('Adelaide Antunes', 'Desenvolvedora', '', '', '', ''),
+						new Fundador('Adelaide Antunes', 'Desenvolvedora', '', '', '', '')
+				);	
+				
+				foreach ($fundadores as $f) :
+			?>			
+			
+			<div class="col-sm-3">
+				<div class="team-member">
+					<img class="img-responsive img-circle img-fundadores" alt=""
+						src="<?php echo $f->urlImagem?>">
+					<h4><?php echo $f->nome?></h4>
+					<p class="text-muted"><?php echo $f->papel?></p>
+					<ul class="list-inline social-buttons">
+						<li><a href="#"><i class="fa fa-twitter"><?php echo $f->twitter?></i></a></li>
+						<li><a href="#"><i class="fa fa-facebook"><?php echo $f->facebook?></i></a></li>
+						<li><a href="#"><i class="fa fa-linkedin"><?php echo $f->linkedin?></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<?php endforeach;?>
+		</div>
 	</div>
-
-	<?php 
-	
-	include 'shared/_footer.php';
-	include 'shared/_scripts.php';
-	
-	?>
-	
-	<!-- script src="assets/js/fundadores/agency.js"></script-->
-</body>
-</html>
