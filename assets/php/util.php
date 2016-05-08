1bc4dfc4646d6fa9e0ce5811a7b0604f91b739fa
Jef <Jef@192.168.15.4>
2016-05-08 13:49:43 -0300

    pequenas alteraçoes no arquivo

--- assets/php/util.php
+++ assets/php/util.php
@@ -1,99 +1,84 @@
 <?php
 	require_once 'conexao.php';
-	$pdo = conectar();
-	
-	Error_reporting(0);
-	if(isset($_GET['acao'])){
-		$acao = $_GET['acao'];
+		function getdados($acao){
+			$pdo = conectar();
+		if(isset($acao)){
+				//sleep(2);
+			
+			if($acao == 'getDestaques'){
+				
+				$listar ="select 	a.cod_animal,
+									a.nom_animal,
+									a.desc_animal,
+									f.cod_foto,
+									f.nom_foto,
+									f.id_foto_pri
+												
+									from 	tb_animal a , tb_foto f
+									where 	a.cod_animal = f.cod_animal
+									and   	f.id_foto_pri = 's'";
 		
-	}else{
-		echo"Não há ação selecionada";
-	}
-	
-	if(isset($acao)){		
-		if($acao == 'getDestaque'){
-			$listar =$pdo->query("SELECT 	A.COD_ANIMAL,
-											A.NOM_ANIMAL,
-											A.DESC_ANIMAL,
-											F.COD_FOTO,
-											F.NOM_FOTO,
-											F.ID_FOTO_PRI
-											
-								FROM 		TB_ANIMAL A , TB_FOTO F
-								WHERE 		A.COD_ANIMAL = F.COD_ANIMAL
-								AND   		F.ID_FOTO_PRI = 'S'");
+				$stmdestaque = $pdo->prepare($listar);
+				$stmdestaque-> execute();
+				if(!$stmdestaque){
+					echo "não há destaques a serem exibidos";
+				}else{	
+					while($rowdestaque = $stmdestaque->fetch()){
+					echo "<figure id ='perfil' class='effect-oscar wowload fadein'>
+							<img src='images/profile/user_1/pet_$rowdestaque[cod_animal]/book/$rowdestaque[nom_foto].jpg'/>
+							<figcaption>
+							<h2>$rowdestaque[nom_animal]</h2>
+							<p> $rowdestaque[desc_animal]</br>
+							<a href='#conteudo' id='animal-filtro' data-value='" .$rowdestaque['cod_animal']. "'>Perfil</a>
+							</figcaption>
+							</figure>";					
+					}
+				} 
+			}/*fim get destaque*/
+			
+			if($acao == 'getperfil'){
+				sleep(2);			
+				$codanimal = $_get['cod_animal'];	
+
+				$sql = "select 	a.cod_animal,
+								a.nom_animal,
+								a.desc_perfil,
+								a.idade,
+								f.cod_foto,
+								f.nom_foto,
+								u.telefone,
+								u.email,
+								u.nom_usuario,							
+								case 
+								  when ind_porte = 1 then 'pequeno'
+								  when ind_porte = 2 then 'medio'
+								  when ind_porte = 3 then 'grade'
+								  when ind_porte = 4 then 'gigante'
+								  else 'indefinido'
+
+
+								end as ind_porte,
+								case 
+								  when ind_sexo  = 1 then 'femea'
+								  when ind_sexo  = 2 then 'macho'
+								  else 'indefinido'
+
+								end as ind_sexo							
+
 								
-			
-			if ($listar ==""){
-				echo 0;
-			}else{	
-				echo json_encode($listar->fetchAll(PDO::FETCH_OBJ));
-			}
-		}
-
-		if($acao == 'getPerfil'){
-			sleep(2);			
-			$codAnimal = $_GET['cod_animal'];	
-
-			$sql = "SELECT 	A.COD_ANIMAL,
-							A.NOM_ANIMAL,
-							A.DESC_PERFIL,
-							A.IDADE,
-							F.COD_FOTO,
-							F.NOM_FOTO,
-							U.TELEFONE,
-							U.EMAIL,
-							U.NOM_USUARIO,							
-							case 
-							  when IND_PORTE = 1 then 'Pequeno'
-							  when IND_PORTE = 2 then 'Medio'
-							  when IND_PORTE = 3 then 'Grade'
-							  when IND_PORTE = 4 then 'Gigante'
-							  else 'Indefinido'
-
-
-							end as IND_PORTE,
-							case 
-							  when IND_SEXO  = 1 then 'Femea'
-							  when IND_SEXO  = 2 then 'Macho'
-							  else 'Indefinido'
-
-							end as IND_SEXO							
-
-							
-					FROM 	TB_ANIMAL A, TB_FOTO F,TB_USUARIO U
-					WHERE 	A.COD_ANIMAL = ?
-					AND     A.COD_USUARIO = U.COD_USUARIO
-					AND     A.COD_ANIMAL = F.COD_ANIMAL AND
-							F.ID_FOTO_PRI = 'S'";  
-							
-			$stm = $pdo->prepare($sql);
-			$stm-> bindValue(1,$codAnimal);
-			$stm-> execute();
-			echo json_encode($stm->fetchAll(PDO::FETCH_OBJ));			
-	
-		}
+						from 	tb_animal a, tb_foto f,tb_usuario u
+						where 	a.cod_animal = ?
+						and     a.cod_usuario = u.cod_usuario
+						and     a.cod_animal = f.cod_animal and
+								f.id_foto_pri = 's'";  
+								
+				$stm = $pdo->prepare($sql);
+				$stm-> bindvalue(1,$codanimal);
+				$stm-> execute();
+				echo json_encode($stm->fetchall(pdo::fetch_obj));			
 		
-		
-		if($acao == 'getCidade' ){
-			$sg_uf = $_GET['cod_estado'];
-			$sg_uf = trim(str_replace(".", "", $sg_uf));
-			$sql = "select   distinct
-							 a.cod_cidade,
-							 a.cod_estado,
-							 c.nom_cidade
-					  from  tb_animal a 
-					  inner join tb_estado e on  (a.cod_estado = e.cod_estado)
-					  inner join tb_cidade c on  (a.cod_cidade = c.cod_cidade)
-					  where  e.sg_uf =?";  
-							
-			$stm = $pdo->prepare($sql);
-			$stm-> bindValue(1,$sg_uf);
-			$stm-> execute();
-			
-			echo json_encode($stm->fetchAll(PDO::FETCH_OBJ));
-			
-			
-		}	
-	}
+			}	
+		}
+	}	
+
 ?>
