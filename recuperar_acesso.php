8fb0a3de55efbceb88077fab7c1ef8ea81b87caf
Jef <Jef@192.168.15.4>
2016-05-08 13:40:16 -0300

    arquivo php responsavel por enviar um email de recuperação de senha
    (alteracao futura)

---
+++ recuperar_acesso.php
@@ -0,0 +1,26 @@
+<title>Miaudote | Recuperar Acesso </title>
+<div id="remove">
+	<div id="acesso">
+		<form name="revovery-form" class="login-form" action="" method="post">
+			<div class="header">
+				<h1 align="center">Recuperar Senha </h1>
+				<span>Para recuperar sua senha, digite seu email no campo abaixo</span>
+			</div>
+
+			<div class="content">
+				<input name="useremail" type="email" class="input usermail"
+					placeholder="Email" required />
+				<div class="user-icon"></div>
+			</div>
+			<div class="footer">
+				<p>
+				<a href="#" class="button">Recuperar</a>
+				</p>
+			</div>
+			<div class="footer">				
+					<i class="navigation"><a href="index.php?page=login" id="login" data-toggle="ignoreScroll" data-link='recuperar_acesso'> Voltar </a></i>
+			</div>
+		</form>
+	</div>
+</div>
+
