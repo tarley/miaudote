f5c735e5065c5ab8af10538fe836bf7015e39f2a
Jef <Jef@192.168.15.4>
2016-05-08 13:34:37 -0300

    arquivo php externo que exibe a lista de x animais recem adotados

---
+++ recem_adotados.php
@@ -0,0 +1,11 @@
+
+	<input type='hidden' id='current_page' />
+	<input type='hidden' id='show_per_page' />
+	<!-- Galeria de destaques-->
+	<div id="recem_adotados" class=" clearfix grid row">
+		<div id="recem_adotados-image" class="scroll col-md-8 col-md-offset-2">
+		   <p>Não há recem adotados para exibir</p>
+		</div>
+	</div>
+	<div id='page_navigation' class="nav-pagination"></div>
+
