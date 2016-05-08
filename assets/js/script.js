0c5db4176c75bfc67f7ee82cff3a4233dc9e1659
Jef <Jef@192.168.15.4>
2016-05-08 13:45:48 -0300

    pequenas adaptações para impedir que o scroll suave da tela ocorra sob q
    qualquer ancora existente na pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,55 @@
  jQuery(document).ready(function($) {	 
 	$(document).on('click','.scroll a, .navbar-brand, .gototop', function(event){ 
-		event.preventDefault();
-		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 600,'swing');
-		$(".scroll li").removeClass('active');
-		$(this).parents('li').toggleClass('active');
-    });
+		var id = $(this).data("toggle");
+		if(id!="ignoreScroll"){
+			if(id!="tab"){
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+				$("li").removeClass('active');
+				$(this).parents('li').toggleClass('active');
+			}
+		}
+	 });
+  });
 	
+
+
+
+
+$(function() {
+	$('.navigation a ').click(function(e) {
+
+		href = $(this).attr("href");
+		pagina = $(this).attr("id");
+		
+		loadContent(pagina,"avancar");
+		
+
+		history.pushState('','New URL: '+href, href);
+		e.preventDefault();
+	});
+	
+	window.onpopstate = function(event) {
+
+		console.log("pathname: "+location.pathname.search);
+		retornar = location.search.substr(6);
+		loadContent(retornar,"voltar");
+	};
+
 });
+
+function loadContent(pagina,destino){
+	
+	if(destino =="avancar"){
+		$('#conteudo-sub').fadeIn(1000).load(pagina+'.php');
+	}else{
+		$('#conteudo-sub').fadeIn(1000).load(pagina+'.php');
+	}
+	
+	$('li').removeClass('current');
+	$('a[href="index.php?page='+pagina+'"]').parent().addClass('current');
+	
+}
 
 
 var wow = new WOW(
