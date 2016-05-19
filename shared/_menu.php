c14bb3b30c010673f9a4f45799f464e8acbd549e
06777545662 <06777545662@SL-LAB6-09008.newtonpaiva.edu.br>
2016-05-18 22:13:14 -0300

    Correção do botão Inicio.

--- shared/_menu.php
+++ shared/_menu.php
@@ -6,8 +6,7 @@
 			<div class="container">
 				<div class="navbar-header">
 					<!-- Logo Starts -->
-					<a class="navbar-brand" href="#home"><img src="images/logo2.png"
-						alt="logo"></a>
+					<a class="navbar-brand" href="/"><img src="images/logo2.png" alt="logo"></a>
 					<!-- #Logo Ends -->
 					<button type="button" class="navbar-toggle collapsed"
 						data-toggle="collapse" data-target=".navbar-collapse">
@@ -20,7 +19,7 @@
 				<!-- Nav Starts -->
 				<div class="navbar-collapse  collapse">
 					<ul class="nav navbar-nav navbar-right">
-						<li class="nav scroll active"><a href="#body" class="menu-topo" id="menu-topo-inicio">Inicio</a></li>
+						<li class="nav scroll active"><a href="/" class="menu-topo" id="menu-topo-inicio">Inicio</a></li>
 						<li class="nav scroll"><a href="#filtro" class="menu-topo" id="menu-topo-buscar">Busca</a></li>
 						<li class="nav scroll"><a href="#about" class="menu-topo" id="menu-topo-quem-somos">Quem Somos</a></li>
 						<li class="nav scroll"><a href="#comoAdotar" class="menu-topo" id="menu-topo-como-adotar">Como Adotar</a></li>
