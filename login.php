5977dcc0dfeea1806fac4594e1977ae953d9e89b
julianosavio <juliano.savio@gmail.com>
2016-06-10 12:47:57 -0300

    Ajuste no titulo da pagina
    Ajuste no titulo da pagina retirando o mesmo antes da função do php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
-
-<title>Miaudote | Login</title>
 <?php global $usuario;
 	  global $senha ?>
+
+<title>Miaudote | Login</title>
 <div id="remove">
 	<div id="acesso">
 		<form name="login-form" class="login-form" id="formulario-login" action="valida_acesso.php" method="POST">
